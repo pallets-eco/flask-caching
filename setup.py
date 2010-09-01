71e37a0bb76fad370a736745e2b5e35d7b92a901
Thadeus Burgess <thadeusb@thadeusb.com>
2010-09-01 13:37:29 -0500

    R-0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Cache',
-    version='0.2',
+    version='0.3',
     url='http://github.com/thadeusb/flask-cache',
     license='BSD',
     author='Thadeus Burgess',
