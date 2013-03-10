9e7ce27fb73e39b054a70a48f33e03c60e7e3dcc
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2013-03-10 12:48:52 +0100

    cache more set/get exception in backend

--- flask_cache/__init__.py
+++ flask_cache/__init__.py
@@ -215,9 +215,8 @@ class Cache(object):
                 if callable(unless) and unless() is True:
                     return f(*args, **kwargs)
 
-                cache_key = decorated_function.make_cache_key(*args, **kwargs)
-
                 try:
+                    cache_key = decorated_function.make_cache_key(*args, **kwargs)
                     rv = self.cache.get(cache_key)
                 except Exception as e:
                     if current_app.debug:
@@ -419,9 +418,8 @@ class Cache(object):
                 if callable(unless) and unless() is True:
                     return f(*args, **kwargs)
 
-                cache_key = decorated_function.make_cache_key(f, *args, **kwargs)
-
                 try:
+                    cache_key = decorated_function.make_cache_key(f, *args, **kwargs)
                     rv = self.cache.get(cache_key)
                 except Exception as e:
                     if current_app.debug:
@@ -523,10 +521,15 @@ class Cache(object):
 
         _fname = function_namespace(f, args)
 
-        if not args and not kwargs:
-            version_key = self._memvname(_fname)
-            version_data = self.memoize_make_version_hash()
-            self.cache.set(version_key, version_data)
-        else:
-            cache_key = f.make_cache_key(f.uncached, *args, **kwargs)
-            self.cache.delete(cache_key)
+        try:
+            if not args and not kwargs:
+                version_key = self._memvname(_fname)
+                version_data = self.memoize_make_version_hash()
+                self.cache.set(version_key, version_data)
+            else:
+                cache_key = f.make_cache_key(f.uncached, *args, **kwargs)
+                self.cache.delete(cache_key)
+        except Exception as e:
+            if current_app.debug:
+                raise e
+            logger.exception("Exception possibly due to cache backend.")
