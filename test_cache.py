3d5c46947bc6f0421818ec4d9c3031732932e785
Thadeus Burgess <thadeusb@thadeusb.com>
2013-03-23 23:20:07 -0500

    Fix redis unit test

--- test_cache.py
+++ test_cache.py
@@ -13,15 +13,6 @@ if sys.version_info < (2,7):
 else:
     import unittest
 
-if 'TRAVIS' in os.environ:
-    try:
-        import redis
-        has_redis = True
-    except ImportError:
-        has_redis = False
-else:
-    has_redis = False
-
 class CacheTestCase(unittest.TestCase):
 
     def _set_app_config(self, app):
@@ -506,47 +497,13 @@ class CacheTestCase(unittest.TestCase):
         from werkzeug.contrib.cache import SimpleCache
         assert isinstance(self.app.extensions['cache'][cache], SimpleCache)
 
-    @unittest.skipUnless(has_redis, "requires Redis")
-    def test_20_redis_url_default_db(self):
-        config = {
-            'CACHE_TYPE': 'redis',
-            'CACHE_REDIS_URL': 'redis://localhost:6379',
-        }
-        cache = Cache()
-        cache.init_app(self.app, config=config)
-        from werkzeug.contrib.cache import RedisCache
-        assert isinstance(self.app.extensions['cache'], RedisCache)
-        rconn = self.app.extensions['cache'] \
-                    ._client.connection_pool.get_connection('foo')
-        assert rconn.db == 0
-
-    @unittest.skipUnless(has_redis, "requires Redis")
-    def test_21_redis_url_custom_db(self):
-        config = {
-            'CACHE_TYPE': 'redis',
-            'CACHE_REDIS_URL': 'redis://localhost:6379/2',
-        }
-        cache = Cache()
-        cache.init_app(self.app, config=config)
-        rconn = self.app.extensions['cache'] \
-                    ._client.connection_pool.get_connection('foo')
-        assert rconn.db == 2
-
-    @unittest.skipUnless(has_redis, "requires Redis")
-    def test_22_redis_url_explicit_db_arg(self):
-        config = {
-            'CACHE_TYPE': 'redis',
-            'CACHE_REDIS_URL': 'redis://localhost:6379/2',
-            'CACHE_REDIS_DB': 1,
-        }
-        cache = Cache()
-        cache.init_app(self.app, config=config)
-        rconn = self.app.extensions['cache'] \
-                    ._client.connection_pool.get_connection('foo')
-        assert rconn.db == 1
-
-
 if 'TRAVIS' in os.environ:
+    try:
+        import redis
+        has_redis = True
+    except ImportError:
+        has_redis = False
+
     class CacheMemcachedTestCase(CacheTestCase):
         def _set_app_config(self, app):
             app.config['CACHE_TYPE'] = 'memcached'
@@ -560,6 +517,45 @@ if 'TRAVIS' in os.environ:
     class CacheRedisTestCase(CacheTestCase):
         def _set_app_config(self, app):
             app.config['CACHE_TYPE'] = 'redis'
+
+        @unittest.skipUnless(has_redis, "requires Redis")
+        def test_20_redis_url_default_db(self):
+            config = {
+                'CACHE_TYPE': 'redis',
+                'CACHE_REDIS_URL': 'redis://localhost:6379',
+            }
+            cache = Cache()
+            cache.init_app(self.app, config=config)
+            from werkzeug.contrib.cache import RedisCache
+            assert isinstance(self.app.extensions['cache'][cache], RedisCache)
+            rconn = self.app.extensions['cache'][cache] \
+                        ._client.connection_pool.get_connection('foo')
+            assert rconn.db == 0
+
+        @unittest.skipUnless(has_redis, "requires Redis")
+        def test_21_redis_url_custom_db(self):
+            config = {
+                'CACHE_TYPE': 'redis',
+                'CACHE_REDIS_URL': 'redis://localhost:6379/2',
+            }
+            cache = Cache()
+            cache.init_app(self.app, config=config)
+            rconn = self.app.extensions['cache'][cache] \
+                        ._client.connection_pool.get_connection('foo')
+            assert rconn.db == 2
+
+        @unittest.skipUnless(has_redis, "requires Redis")
+        def test_22_redis_url_explicit_db_arg(self):
+            config = {
+                'CACHE_TYPE': 'redis',
+                'CACHE_REDIS_URL': 'redis://localhost:6379/2',
+                'CACHE_REDIS_DB': 1,
+            }
+            cache = Cache()
+            cache.init_app(self.app, config=config)
+            rconn = self.app.extensions['cache'][cache] \
+                        ._client.connection_pool.get_connection('foo')
+            assert rconn.db == 1
 
 
     class CacheFilesystemTestCase(CacheTestCase):
