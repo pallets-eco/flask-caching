40417abd38328e5ee9b00617c435f9b3d1a9e0b6
Jens Geyti <jens.geyti@performancehorizon.com>
2016-12-07 12:15:28 +0000

    Change redis import mechanism to avoid ignoring ImportErrors

--- flask_caching/backends.py
+++ flask_caching/backends.py
@@ -65,38 +65,37 @@ def filesystem(app, config, args, kwargs):
     kwargs.update(dict(threshold=config['CACHE_THRESHOLD']))
     return FileSystemCache(*args, **kwargs)
 
-# RedisCache is supported since Werkzeug 0.7.
-try:
+def redis(app, config, args, kwargs):
+    # RedisCache is supported since Werkzeug 0.7. 
+    # Place RedisCache import here to not interfere with the
+    # rest of the cache backends for users running Werkzeug <0.7
     from werkzeug.contrib.cache import RedisCache
     from redis import from_url as redis_from_url
-except ImportError:
-    pass
-else:
-    def redis(app, config, args, kwargs):
-        kwargs.update(dict(
-            host=config.get('CACHE_REDIS_HOST', 'localhost'),
-            port=config.get('CACHE_REDIS_PORT', 6379),
-        ))
-        password = config.get('CACHE_REDIS_PASSWORD')
-        if password:
-            kwargs['password'] = password
+    
+    kwargs.update(dict(
+        host=config.get('CACHE_REDIS_HOST', 'localhost'),
+        port=config.get('CACHE_REDIS_PORT', 6379),
+    ))
+    password = config.get('CACHE_REDIS_PASSWORD')
+    if password:
+        kwargs['password'] = password
 
-        key_prefix = config.get('CACHE_KEY_PREFIX')
-        if key_prefix:
-            kwargs['key_prefix'] = key_prefix
+    key_prefix = config.get('CACHE_KEY_PREFIX')
+    if key_prefix:
+        kwargs['key_prefix'] = key_prefix
 
-        db_number = config.get('CACHE_REDIS_DB')
-        if db_number:
-            kwargs['db'] = db_number
+    db_number = config.get('CACHE_REDIS_DB')
+    if db_number:
+        kwargs['db'] = db_number
 
-        redis_url = config.get('CACHE_REDIS_URL')
-        if redis_url:
-            kwargs['host'] = redis_from_url(
-                redis_url,
-                db=kwargs.pop('db', None),
-            )
+    redis_url = config.get('CACHE_REDIS_URL')
+    if redis_url:
+        kwargs['host'] = redis_from_url(
+            redis_url,
+            db=kwargs.pop('db', None),
+        )
 
-        return RedisCache(*args, **kwargs)
+    return RedisCache(*args, **kwargs)
 
 
 class SpreadSASLMemcachedCache(SASLMemcachedCache):
