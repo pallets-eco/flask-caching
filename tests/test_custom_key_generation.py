0cda1b4ed11837e08b6f149191e18467da947471
Stas Kaledin <staskaledin@gmail.com>
2016-12-26 12:47:36 +0300

    Added test for regression

--- tests/test_custom_key_generation.py
+++ tests/test_custom_key_generation.py
@@ -19,6 +19,10 @@ class TestCustomKeyGeneration(TestCase):
             def method(self, param, notUsedParam):
                 return notUsedParam
 
+            @cache.memoize(10**6)
+            def not_exclude_method(self, param1, param2):
+                return self._value
+
         self.Test = Test
         self.cache = cache
 
@@ -35,3 +39,18 @@ class TestCustomKeyGeneration(TestCase):
 
         self.assertEqual("2", ins.method("1", "2"))
         self.assertEqual("3", ins.method("2", "3"))
+
+    def testNotExcludeParam(self):
+
+        ins = self.Test()
+
+        self.assertEqual(1, ins.not_exclude_method("1", "2"))
+        ins._value = 2
+        self.assertEqual(1, ins.not_exclude_method("1", "2"))
+        self.assertEqual(2, ins.not_exclude_method("2", "2"))
+
+        ins._value = 3
+        self.assertEqual(2, ins.not_exclude_method("2", "2"))
+
+        self.cache.delete_memoized(self.Test.not_exclude_method)
+        self.assertEqual(3, ins.not_exclude_method("2", "2"))
