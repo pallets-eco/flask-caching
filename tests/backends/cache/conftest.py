3d9317f7b2b227e514ddcac2682fbce7efce53ac
Dave Chevell <dchevell@atlassian.com>
2018-07-10 08:35:02 +1000

    Added werkzeug test cases for cache backend

---
+++ tests/backends/cache/conftest.py
@@ -0,0 +1,35 @@
+import os
+
+import pytest
+
+# build the path to the uwsgi marker file
+# when running in tox, this will be relative to the tox env
+filename = os.path.join(
+    os.environ.get('TOX_ENVTMPDIR', ''),
+    'test_uwsgi_failed'
+)
+
+try:
+    __import__('pytest_xprocess')
+except ImportError:
+    @pytest.fixture(scope='session')
+    def xprocess():
+        pytest.skip('pytest-xprocess not installed.')
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """``uwsgi --pyrun`` doesn't pass on the exit code when ``pytest`` fails,
+    so Tox thinks the tests passed. For UWSGI tests, create a file to mark what
+    tests fail. The uwsgi Tox env has a command to read this file and exit
+    appropriately.
+    """
+    outcome = yield
+    report = outcome.get_result()
+
+    if item.cls.__name__ != 'TestUWSGICache':
+        return
+
+    if report.failed:
+        with open(filename, 'a') as f:
+            f.write(item.name + '\n')
