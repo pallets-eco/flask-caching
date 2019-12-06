e340aaf78faa66d2c77dfc01c48c26da5d0a3ecd
huangsong <huangsong@leyantech.com>
2019-12-06 14:01:07 +0800

    fix protocol level when sys_info >= 3.8w

--- tests/test_pickle.py
+++ tests/test_pickle.py
@@ -1,17 +1,22 @@
+import sys
 import _pickle
 
 
 def test_pickle_highest_protocol():
+    if sys.version_info >= (3, 8):
+        level = 6
+    else:
+        level = 5
     # test wrong protocol level
     try:
-        _pickle.dumps({"a": 1}, 5)
+        _pickle.dumps({"a": 1}, level)
     except ValueError as e:
-        assert str(e) == "pickle protocol must be <= 4"
+        assert str(e) == f"pickle protocol must be <= {level - 1}"
     else:
         assert 1 == 2
 
     try:
-        _pickle.dumps({"a": 1}, 4)
+        _pickle.dumps({"a": 1}, level - 1)
         e = None
     except ValueError as e:
         e = str(e)
