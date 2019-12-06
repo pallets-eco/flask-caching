ae204e676bf501d395006ce64934ce3ade8a8566
huangsong <huangsong@leyantech.com>
2019-12-06 14:08:05 +0800

    fix in python 3.5

--- tests/test_pickle.py
+++ tests/test_pickle.py
@@ -8,15 +8,16 @@ def test_pickle_highest_protocol():
     else:
         level = 5
     # test wrong protocol level
+    low_level = level - 1
     try:
         _pickle.dumps({"a": 1}, level)
     except ValueError as e:
-        assert str(e) == f"pickle protocol must be <= {level - 1}"
+        assert str(e) == "pickle protocol must be <= {}".format(low_level)
     else:
         assert 1 == 2
 
     try:
-        _pickle.dumps({"a": 1}, level - 1)
+        _pickle.dumps({"a": 1}, low_level)
         e = None
     except ValueError as e:
         e = str(e)
