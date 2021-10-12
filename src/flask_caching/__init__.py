77a4763070effe0d322488c109673e7457643ea9
huangsong <huangsong@leyantech.com>
2021-10-12 21:08:25 +0800

    add test

--- src/flask_caching/__init__.py
+++ src/flask_caching/__init__.py
@@ -152,13 +152,6 @@ def make_template_fragment_key(fragment_name: str, vary_on: List[str] = None) ->
     return TEMPLATE_FRAGMENT_KEY_TEMPLATE % (fragment_name, "_".join(vary_on))
 
 
-def call_fn(fn, *args, **kwargs):
-    ensure_sync = getattr(current_app, "ensure_sync", None)
-    if ensure_sync is not None:
-        return current_app.ensure_sync(fn)(*args, **kwargs)
-    return fn(*args, **kwargs)
-
-
 class Cache:
     """This class is used to control the cache objects."""
 
@@ -267,6 +260,12 @@ class Cache:
             app, config, cache_args, cache_options
         )
         self.app = app
+
+    def _call_fn(self, fn, *args, **kwargs):
+        ensure_sync = getattr(self.app, "ensure_sync", None)
+        if ensure_sync is not None:
+            return self.app.ensure_sync(fn)(*args, **kwargs)
+        return fn(*args, **kwargs)
 
     @property
     def cache(self) -> SimpleCache:
@@ -437,7 +436,7 @@ class Cache:
             def decorated_function(*args, **kwargs):
                 #: Bypass the cache entirely.
                 if self._bypass_cache(unless, f, *args, **kwargs):
-                    return call_fn(f, *args, **kwargs)
+                    return self._call_fn(f, *args, **kwargs)
 
                 nonlocal source_check
                 if source_check is None:
@@ -482,10 +481,10 @@ class Cache:
                     if self.app.debug:
                         raise
                     logger.exception("Exception possibly due to cache backend.")
-                    return call_fn(f, *args, **kwargs)
+                    return self._call_fn(f, *args, **kwargs)
 
                 if not found:
-                    rv = call_fn(f, *args, **kwargs)
+                    rv = self._call_fn(f, *args, **kwargs)
 
                     if response_filter is None or response_filter(rv):
                         try:
@@ -907,7 +906,7 @@ class Cache:
             def decorated_function(*args, **kwargs):
                 #: bypass cache
                 if self._bypass_cache(unless, f, *args, **kwargs):
-                    return call_fn(f, *args, **kwargs)
+                    return self._call_fn(f, *args, **kwargs)
 
                 nonlocal source_check
                 if source_check is None:
@@ -949,10 +948,10 @@ class Cache:
                     if self.app.debug:
                         raise
                     logger.exception("Exception possibly due to cache backend.")
-                    return call_fn(f, *args, **kwargs)
+                    return self._call_fn(f, *args, **kwargs)
 
                 if not found:
-                    rv = call_fn(f, *args, **kwargs)
+                    rv = self._call_fn(f, *args, **kwargs)
 
                     if response_filter is None or response_filter(rv):
                         try:
