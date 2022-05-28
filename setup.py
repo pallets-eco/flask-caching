cd5aa95ddc5b411c9000b2f40539f830227dbe20
ix5 <ix5@users.noreply.github.com>
2022-05-28 23:34:19 +0200

    setup.py: Add 'cachelib' dependency (#352)
    Since #308 added a hard requirement for `cachelib`, add it
    to user-facing `setup.py` as well (it was previously
    only added to dev-dependencies).

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask-Caching",
-    install_requires=["Flask"],
+    install_requires=["cachelib", "Flask"],
     tests_require=[
         "pytest",
         "pytest-xprocess",
