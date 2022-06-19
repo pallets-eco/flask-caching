73b34cf77b19a94ffbd9a47e57fa98ff817a8188
Gabriel Fioravante <gfvante@pm.me>
2022-06-19 17:28:30 -0300

    remove file cache overrides and tests

--- src/flask_caching/backends/filesystemcache.py
+++ src/flask_caching/backends/filesystemcache.py
@@ -10,9 +10,6 @@
 """
 import hashlib
 import logging
-import os
-import pickle
-from time import time
 
 from cachelib import FileSystemCache as CachelibFileSystemCache
 
@@ -77,77 +74,3 @@ class FileSystemCache(BaseCache, CachelibFileSystemCache):
             )
         )
         return cls(*args, **kwargs)
-
-    def _list_dir(self):
-        """return a list of (fully qualified) cache filenames"""
-        mgmt_files = [
-            self._get_filename(name).split("/")[-1] for name in (self._fs_count_file,)
-        ]
-        return [
-            os.path.join(self._path, fn)
-            for fn in os.listdir(self._path)
-            if not fn.endswith(self._fs_transaction_suffix) and fn not in mgmt_files
-        ]
-
-    def _prune(self):
-        if self._threshold == 0 or not self._file_count > self._threshold:
-            return
-
-        entries = self._list_dir()
-        nremoved = 0
-        now = time()
-        for idx, fname in enumerate(entries):
-            try:
-                remove = False
-                with open(fname, "rb") as f:
-                    expires = pickle.load(f)
-                remove = (expires != 0 and expires <= now) or idx % 3 == 0
-                if remove:
-                    os.remove(fname)
-                    nremoved += 1
-            except OSError:
-                pass
-        self._update_count(value=len(self._list_dir()))
-        logger.debug("evicted %d key(s)", nremoved)
-
-    def get(self, key):
-        result = None
-        expired = False
-        hit_or_miss = "miss"
-        filename = self._get_filename(key)
-        try:
-            with open(filename, "rb") as f:
-                pickle_time = pickle.load(f)
-                expired = pickle_time != 0 and pickle_time < time()
-                if not expired:
-                    hit_or_miss = "hit"
-                    result = pickle.load(f)
-            if expired:
-                self.delete(key)
-        except FileNotFoundError:
-            pass
-        except (OSError, pickle.PickleError) as exc:
-            logger.error("get key %r -> %s", key, exc)
-        expiredstr = "(expired)" if expired else ""
-        logger.debug("get key %r -> %s %s", key, hit_or_miss, expiredstr)
-        return result
-
-    def has(self, key):
-        result = False
-        expired = False
-        filename = self._get_filename(key)
-        try:
-            with open(filename, "rb") as f:
-                pickle_time = pickle.load(f)
-            expired = pickle_time != 0 and pickle_time < time()
-            if expired:
-                self.delete(key)
-            else:
-                result = True
-        except FileNotFoundError:
-            pass
-        except (OSError, pickle.PickleError) as exc:
-            logger.error("get key %r -> %s", key, exc)
-        expiredstr = "(expired)" if expired else ""
-        logger.debug("has key %r -> %s %s", key, result, expiredstr)
-        return result
