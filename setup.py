7a49ad2c0f4a82093e0e42a1014b8709cfcf4959
Fabian Dill <Berserker66@users.noreply.github.com>
2022-07-30 16:37:11 +0200

    Relax Flask version restriction (#386)
    * Relax Flask version restriction
    
    ... to minor version that's compatible
    
    * restrict flask major version only
    
    * add changelog entry
    
    Co-authored-by: Gabriel Fioravante <gfvante@pm.me>

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask-Caching",
-    install_requires=["cachelib >= 0.9.0", "Flask <= 2.1.2"],
+    install_requires=["cachelib >= 0.9.0", "Flask < 3"],
 )
