91ee2c574f789d75cf296afd238530ddb03f9ecc
northernSage <gfvante@gmail.com>
2024-05-19 13:34:13 -0300

    relax cachelib version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask-Caching",
-    install_requires=["cachelib >= 0.9.0, < 0.10.0", "Flask"],
+    install_requires=["cachelib >= 0.9.0", "Flask"],
 )
