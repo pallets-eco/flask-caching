04575dd2a353925ba947b183389a20dc476fc84b
Peter Justin <peter.justin@outlook.com>
2026-08-07 20:40:40 +0200

    fix co/contravariant

--- src/flask_caching/backends/uwsgicache.py
+++ src/flask_caching/backends/uwsgicache.py
@@ -8,7 +8,8 @@ class UWSGICache(_UWSGICache):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         warnings.warn(
             "Importing UWSGICache from flask_caching.backends is deprecated, "
-            "use flask_caching.contrib.uwsgicache.UWSGICache instead",
+            "use flask_caching.contrib.uwsgicache.UWSGICache instead. "
+            "This will be removed in a future version.",
             category=DeprecationWarning,
             stacklevel=2,
         )
